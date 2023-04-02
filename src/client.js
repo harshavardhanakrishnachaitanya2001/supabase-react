26d828a941151fcf52ee09e360101419dfe9abd6
harshavardhanakrishnachaitanya2001 <pvageta02@gmail.com>
2023-04-02 19:00:05 +0530

    Made a blog website with react and supabase

---
+++ src/client.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+export const supabase = createClient(
+    "https://uabxbioiiswprbvesxqc.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhYnhiaW9paXN3cHJidmVzeHFjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3OTk3ODExNywiZXhwIjoxOTk1NTU0MTE3fQ.52zfoD81g3nxY35-IeuzAZ4XNllRG2ap9zanK8Z-aZc"
+
+    
+) 
